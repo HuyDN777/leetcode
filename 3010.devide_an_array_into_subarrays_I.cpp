9667022176e2aeb72e68bc15747617b88b8b450d
HuyDN <huydn777@gmail.com>
2026-02-01 21:19:55 +0700

    01/02/2026

---
+++ 3010.devide_an_array_into_subarrays_I.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    public:
+        int minimumCost(vector<int>& nums) {
+            int sum = nums[0];
+            int min1 = INT_MAX;
+            int min2 = INT_MAX;
+            
+            for (size_t i = 1; i < nums.size(); ++i) {
+                if (nums[i] < min1) {
+                    min2 = min1;
+                    min1 = nums[i];
+                } else if (nums[i] < min2) min2 = nums[i];
+            }
+            
+            return sum + min1 + min2;
+        }
+    };
